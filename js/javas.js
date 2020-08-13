b347cb932eb692f55bbb8f97fe27a84c097218f0
cosmin2iordache <cosmin2iordache@gmail.com>
2020-08-13 16:22:12 +0100

    hey

--- js/javas.js
+++ js/javas.js
@@ -1,1 +1,76 @@
-console.log("hi")
+//DAY 6 Higher Order Function
+function doubleMe(x) {
+      return x*2
+}
+
+document.write(doubleMe(100))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DAY 5
+// document.writeln("hello Cosmin")
+
+
+// let testNumber = 100;
+// let number = 0
+// if (testNumber)
+//       console.log("true")
+
+// if (number)
+//       console.log("true")
+//       else console.log("false")
+
+// while (testNumber < 105) {
+//       document.write(testNumber + "<br>")
+//       console.log(testNumber)
+//       testNumber++
+// }
+
+
+
+
+// DAY 4
+// let myNumbers = [1,2,3,4,5,6]
+// let myWords = ["one", "two", "five"]
+
+// let a = 2
+// let b = "2"
+
+// console.log(a+b)
+// //-----------------------------------------------------------------
+// myWords.push("a") 
+// console.log(myWords)
+
+// myWords.pop()
+// myWords.pop()
+// console.log(myWords.pop())
+
+// console.log(myWords.splice(1, 1))
+
+
+
+
+// let cosmin = "coSMin"
+// console.log(cosmin.toUpperCase())
+// // console.log(myNumbers.length)
+// console.log(`------------------------------------------- \n ${myWords}`)
+// console.log(myWords)
+// console.log(JSON.parse((JSON.stringify(myWords))))
+
+
