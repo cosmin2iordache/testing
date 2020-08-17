dbaeaed2cfb780e174961b0dd4ab443fae26c072
cosmin2iordache <cosmin2iordache@gmail.com>
2020-08-17 23:08:20 +0100

    salut

--- js/javas.js
+++ js/javas.js
@@ -1,19 +1,146 @@
-//DAY 6 Higher Order Function
+// class Person {
+//       constructor(x) {
+//             this.name = x
+//       }
 
-// let name = "cosmin"
+//       printNameFromArrow() {
+//             setTimeout(() => console.log("Hi " + this.name), 1000)
+//       }
 
-function repeatName(name) {
-      return function (x) {
-      let repeat = 0      
-      while (repeat < x) {
-            document.write(name + "<br>")
-            repeat++
-            }
-      }
+//       printNameFromFunction() {
+//             setTimeout(function() {console.log("Hi " + this.name)}, 1000)
+//       }
+// }
+
+// let cosmin = new Person("Cosmin")
+// cosmin.printNameFromArrow()
+// cosmin.printNameFromFunction()
+
+
+
+
+
+
+// map and filer => arrows
+// let animals = [
+//       {name: "MAX", age: 4, color: "red"},
+//       {name: "PAX", age: 5, color: "red"},
+//       {name: "FOX", age: 6, color: "blue"},
+//       {name: "DOX", age: 7, color: "yellow"}
+// ]
+
+// console.log(animals.push({name: "ZIX", age: 4, color: "yellow"}))
+// // console.log(animals)
+
+// let names = animals.map(x => x.name)
+
+// let age = animals.filter(x=>x.age >4)
+
+// let color = animals.filter(x => x.color == "red")
+
+
+// let finalFunction = animals.filter(onlyBig).filter(onlyRed).map(onlyNames)
+
+
+// console.log(finalFunction)
+
+
+
+
+
+
+// map and filer
+let animals = [
+      {name: "MAX", age: 4, color: "red"},
+      {name: "PAX", age: 5, color: "red"},
+      {name: "FOX", age: 6, color: "blue"},
+      {name: "DOX", age: 7, color: "yellow"}
+]
+
+console.log(animals.push({name: "ZIX", age: 4, color: "yellow"}))
+
+function onlyNames(x) {
+      return x.name
 }
 
-let max = repeatName("Max")
-document.write(max(3))
+function onlyBig(x) {
+      return x.age >4
+}
+
+function onlyRed(x) {
+      return x.color == "red"
+}
+
+
+let finalFunction = animals.filter(onlyBig).filter(onlyRed).map(onlyNames)
+let finalFunction2 = animals.filter(x => x.age>4).filter(onlyRed).map(onlyNames)
+
+
+console.log(finalFunction)
+console.log(finalFunction2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// map and filer
+// let animals = [
+//       {name: "MAX", age: 4, color: "red"},
+//       {name: "PAX", age: 5, color: "red"},
+//       {name: "FOX", age: 6, color: "blue"},
+//       {name: "DOX", age: 7, color: "yellow"}
+// ]
+
+// console.log(animals.push({name: "ZIX", age: 4, color: "yellow"}))
+// console.log(animals)
+
+// let names = animals.map(onlyNames)
+// function onlyNames(x) {
+//       return x.name
+// }
+
+// let age = animals.filter(onlyBig)
+// function onlyBig(x) {
+//       return x.age >4
+// }
+
+// let color = animals.filter(onlyRed)
+// function onlyRed(x) {
+//       return x.color == "red"
+// }
+
+
+// let finalFunction = animals.filter(onlyBig).filter(onlyRed).map(onlyNames)
+
+
+// console.log(finalFunction)
+
+
+
+
+
+//DAY 6 Higher Order Function
+
+// function repeatName(name) {
+//       return function (x) {
+//       let repeat = 0      
+//       while (repeat < x) {
+//             document.write(name + "<br>")
+//             repeat++
+//       }}}
+
+// let max = repeatName("Max")
+// max(3)
 
 // console.log(max(5))
 
@@ -102,5 +229,6 @@ document.write(max(3))
 // console.log(`------------------------------------------- \n ${myWords}`)
 // console.log(myWords)
 // console.log(JSON.parse((JSON.stringify(myWords))))
+
 
 
